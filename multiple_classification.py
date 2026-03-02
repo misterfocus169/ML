ae6459d28413db49a7b2d57817e6c7968e072f87
misterfocus169 <misterfocus169@gmail.com>
2026-03-02 18:12:38 +0700

    Update multiple_classification.py

--- multiple_classification.py
+++ multiple_classification.py
@@ -101,8 +101,9 @@ if(selected == 'BMI'):
             bmi_predict = 'Obesity'
     
     st.success(bmi_predict)
-        except ValueError:
-            st.error("กรุณากรอกตัวเลขในช่องส่วนสูงและน้ำหนัก")
+except ValueError:
+    st.error("กรุณากรอกตัวเลขในช่องส่วนสูงและน้ำหนัก")
+
 
 
 
